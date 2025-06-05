9e5b1cbb8e2e9aeff2b111b52392f98ab0b31584
Hammad Ahmed <hammadahmed9110@gmail.com>
2025-06-05 15:52:55 +0500

    Mobile Sidebar issue Resolved

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,8 +23,10 @@ export default function RootLayout({
         <div className="min-h-screen flex flex-col bg-[linear-gradient(135deg,_#e0f2fe_0%,_#f0f9ff_20%,_#ffe4e6_40%,_#bae6fd_60%,_#a5f3fc_100%)]">
           <Header isSidebarCollapsed={isSidebarCollapsed} onNavToggle={setIsNavOpen} />
           <div className="flex flex-1 relative">
-            <Sidebar isCollapsed={isSidebarCollapsed} setIsCollapsed={setIsSidebarCollapsed} />
-            <main className={`transition-all duration-300 ease-in-out ${isNavOpen ? 'pt-48' : 'pt-16'} md:pt-16 flex-1 pb-20 md:${isSidebarCollapsed ? 'ml-24' : 'ml-64'} ${isSidebarCollapsed ? 'ml-16' : 'ml-48'}`}>
+            <div className="hidden md:block">
+              <Sidebar isCollapsed={isSidebarCollapsed} setIsCollapsed={setIsSidebarCollapsed} />
+            </div>
+            <main className={`transition-all duration-300 ease-in-out ${isNavOpen ? 'pt-48' : 'pt-16'} md:pt-16 flex-1 pb-20 ${isSidebarCollapsed ? '' : ''} md:${isSidebarCollapsed ? 'ml-24' : 'ml-64'} ml-0`}>
               {children}
               <Footer />
             </main>
